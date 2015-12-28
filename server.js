1c53d41d8f28a63f199c8e750454af9793717089
Christian Ortiz <chris.ortiz.reina@googlemail.com>
2015-12-28 22:44:38 +0100

    basic changes

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var app = express();
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
+
 app.get('/', function (req, res) {
   res.send('demo');
 });
 
-var server = app.listen(server_port,  server_ip_address, function () {
+var server = app.listen(server_port, server_ip_address, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', server_ip_address, server_port);
+  console.log('Started at http://%s:%s', host, port);
 });
