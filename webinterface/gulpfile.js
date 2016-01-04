1615d3560bf87abeae83f68747e3b730b5f3c5d2
Christian Ortiz <chris.ortiz.reina@googlemail.com>
2016-01-05 00:56:10 +0100

    added test + refactoring

--- webinterface/gulpfile.js
+++ webinterface/gulpfile.js
@@ -1,16 +1,43 @@
 var gulp = require('gulp');
 var inject = require('gulp-inject');
 var concat = require('gulp-concat');
+var Server = require('karma').Server;
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
- 
-gulp.task('inject_sources', ['concat_js', 'concat_css'], function () {
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+}); 
+
+/**
+ * Watch for file changes and re-run tests on each change
+ */
+gulp.task('tdd', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start();
+});
+
+gulp.task('inject_sources', ['concat_js', 'concat_css', 'uglify'], function () {
 	var src = gulp.src('./src/index.html');
 	// It's not necessary to read the files (will speed up things), we're only after their paths: 
-	var sources = gulp.src(['./js/vendor.js', './css/vendor.css'], { read: false, cwd: './dist/' });
+	var vendorSources = gulp.src(['./js/vendor.js', './css/vendor.css'], { read: false, cwd: './dist/' });
+	var mainSources = gulp.src(['./js/main.js'], { read: false, cwd: './dist/' });
 
 	return src
-		.pipe(inject(sources, { addRootSlash: false }))
+		.pipe(inject(vendorSources, { addRootSlash: false, name: 'vendor' }))
+		.pipe(inject(mainSources, { addRootSlash: false, name: 'main' } ))
 		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('uglify', function() {
+	return gulp.src('./src/js/tempMon.js')
+    	.pipe(uglify())
+    	.pipe(rename('main.js'))
+    	.pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('concat_js', function() {
@@ -30,4 +57,4 @@ gulp.task('concat_css', function() {
     .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('default', ['concat_css', 'concat_js', 'inject_sources']);
+gulp.task('default', ['uglify', 'concat_css', 'concat_js', 'inject_sources']);
